75120a21a34417d885fac79510611cd0981aec0e
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-02 16:56:18 -0300

    Primeira aula de js

---
+++ 4- Condicionais.js
@@ -0,0 +1,9 @@
+// Condicionais
+
+const idade = 9
+
+if (idade >=18) {
+    console.log("Maior de idade")
+} else {
+    console.log("Menor de idade")
+}
---
+++ 5-if-elif-else.js
@@ -0,0 +1,13 @@
+//Estruturas condicionais
+
+const idade = 15
+
+if (idade>=18) {
+    console.log('Maior de idade')
+} else if (idade>=12) {
+    console.log("Adolescente")
+} else {
+    console.log("Criança")
+} 
+
+//Menor que 2 ano: BEBÊ
