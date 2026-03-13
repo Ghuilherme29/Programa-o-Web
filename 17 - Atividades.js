3d5035980e206727d89207a2d4f9b669fd534842
kaualopes-best <kaua.lopes@ba.estudante.senai.br>
2026-03-13 15:35:05 -0300

    Atividade da aula 4 concluida

--- 17 - Atividades.js
+++ 17 - Atividades.js
@@ -1,25 +1,32 @@
 const listaDeAlunos = [
-    {Nome:"Ana", nota: 6.0},
-    {Nome:"Bruno", nota: 8.0},
-    {Nome:"Carla", nota: 9.0},
-    {Nome:"Andreia", nota: 7.0},
-    {Nome:"Marta", nota: 6.0}
+    {nome:"Ana", nota: 6.0},
+    {nome:"Bruno", nota: 8.0},
+    {nome:"Carla", nota: 9.0},
+    {nome:"Andreia", nota: 7.0},
+    {nome:"Marta", nota: 6.0}
 ]
 
 console.log("Encontre a aluna marta e mostre o nome e a média dela.")
-const UsuarioEncontrado = listaDeAlunos.find(((u) => u.Nome == 'Marta'));
+const UsuarioEncontrado = listaDeAlunos.find(((u) => u.nome == 'Marta'));
 console.log(
-    `Nome: ${UsuarioEncontrado.Nome}\nnota: ${UsuarioEncontrado.nota}`
+    `Nome: ${UsuarioEncontrado.nome}\nnota: ${UsuarioEncontrado.nota}`
 );
 
 console.log("\nMostrando a média geral da turma:")
 const somaNota = listaDeAlunos.reduce(
-    (total,Nome ) => total + Nome.nota /2,0,
+    (total,nome ) => total + nome.nota /2,0,
 );
 console.log(somaNota)
 
 console.log("\nMostrando o nome e a nota dos alunos com a nota abaixo ou igual a 7.0")
-const UsuarioEncontradoPornota = listaDeAlunos.forEach((u) => u.nota <= 7)
-console.log(
-    `Nome: ${UsuarioEncontrado.Nome}\nnota: ${UsuarioEncontrado.nota}`
+const Mnn = listaDeAlunos.filter((nome)=> nome.nota <= 7)
+Mnn.forEach((nome) =>
+    console.log(`${nome.nome} está com ${nome.nota} pontos`)
 )
+
+console.log("\nMostrando o nome e a nota dos alunos com a nota maior ou igual a 7.0")
+const Mnnm = listaDeAlunos.filter((nome)=> nome.nota >= 7)
+Mnnm.forEach((nome) =>
+    console.log(`${nome.nome} está com ${nome.nota} pontos`)
+)
+
