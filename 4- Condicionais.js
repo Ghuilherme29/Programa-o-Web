b1e193eff0f610e6bcbe1b62f499fd459d98c0c4
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 13:51:33 -0300

    base do projeto

--- 4- Condicionais.js
+++
@@ -1,9 +0,0 @@
-// Condicionais
-
-const idade = 9
-
-if (idade >=18) {
-    console.log("Maior de idade")
-} else {
-    console.log("Menor de idade")
-}
--- 8- Laco-while.js
+++
@@ -1,8 +0,0 @@
-// Laços de repetição: While
-
-let = 1
-
-while (i <=5) {
-    console.log(i)
-    i++
-}
