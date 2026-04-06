c41835d04f86edf6d724d0d82df8958a01880712
Aluno <Aluno@C302-LAB-000.senaiba.com.br>
2026-04-06 15:04:53 -0300

    Projeto

---
+++ projeto/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+export default function Footer() {
+    <footer className="footer">
+        <p>
+            &copy; {new Date().getFullYear()} 
+            <br />
+            SENAI
+        </p>
+    </footer>
+}
